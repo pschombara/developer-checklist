8ec49d7c405dce072097beee658bdfbdb8135ba4
Patrick Schombara <pschombara@gmail.com>
2020-08-08 19:52:48 +0200

    feature/prepare
    - Extended config validation.

--- src/js/modules/options/validator.mjs
+++ src/js/modules/options/validator.mjs
@@ -28,6 +28,112 @@ export class Validator {
     }
 }
 
+const checkLists = (data) => {
+
+    return [];
+}
+
+const checkJenkins = (data) => {
+
+    return [];
+}
+
+const checkCategories = (data, optionKey) => {
+    if (false === Array.isArray(data)) {
+        return [{
+            err: `Unsupported type for option ${optionKey}! Expected value is from type array.`,
+        }];
+    }
+
+    let errors = [];
+
+    data.forEach((entry, key) => {
+        if ('string' !== typeof entry) {
+            errors.push({
+                err: `Unsupported type for entry ${optionKey}[${key}]! Expected value from type string.`
+            });
+        }
+    });
+
+    return errors;
+}
+
+const checkJenkinsCategories = (data) => {
+    return checkCategories(data, 'jenkinsCategories');
+}
+
+const checkGitCategories = (data) => {
+    return checkCategories(data, 'gitCategories');
+}
+
+const checkGit = (data) => {
+    if (false === Array.isArray(data)) {
+        return [{
+            err: 'Unsupported type for option git! Expected value is from type array.'
+        }];
+    }
+
+    let errors = [];
+    const requiredKeys = ['domain', 'project'];
+    const types = {
+        domain: 'string',
+        project: 'string',
+    }
+
+    data.forEach((entry, key) => {
+        if ('object' !== typeof entry) {
+            errors.push({
+                err: `Unsupported type for entry git[${key}]! Expected value is from type object.`
+            });
+        } else {
+            for (let requiredKey of requiredKeys) {
+                if (false === entry.hasOwnProperty(requiredKey)) {
+                    errors.push({
+                        err: `Missing key ${requiredKey} for entry git[${key}]!`
+                    });
+                } else if (types[requiredKey] !== typeof entry[requiredKey]) {
+                    errors.push({
+                        err: `Unsupported type for entry git[${key}]! Expected value is form type ${types[requiredKey]}`
+                    });
+                }
+            }
+
+            Object.keys(entry).forEach((name) => {
+                if (-1 === requiredKeys.indexOf(name)) {
+                    errors.push({
+                        err: `Unknown key ${name} for option git[${key}]!`
+                    });
+                }
+            });
+        }
+    });
+
+    return errors;
+}
+
+const checkJira = (data) => {
+    return [];
+}
+
+const checkRocketChat = (data) => {
+    return [];
+}
+
+const checkCheatSheet = (data) => {
+    return [];
+}
+
+const check = {
+    lists: checkLists,
+    jenkins: checkJenkins,
+    jenkinsCategories: checkJenkinsCategories,
+    git: checkGit,
+    gitCategories: checkGitCategories,
+    jira: checkJira,
+    rocketChat: checkRocketChat,
+    cheatSheet: checkCheatSheet
+}
+
 const checkIsObject = (data) => {
     if ('object' !== typeof data) {
         return [{
@@ -61,6 +167,8 @@ const checkRootSchema = (data) => {
             errors.push({
                 err: `Unsupported option "${key}"!`,
             });
+        } else {
+            errors.push(...check[key](data[key]));
         }
     });
 
