6aecfd751f416b634624e6112da1c26f93a8a5aa
Patrick Schombara <pschombara@gmail.com>
2020-08-22 16:11:19 +0200

    jdc-23
    jira generals

--- src/js/modules/options/jira.mjs
+++ src/js/modules/options/jira.mjs
@@ -14,50 +14,15 @@ export class Jira extends SuperJira {
 
     init() {
         this._boards.init(this.options.boards);
-        Object.keys(this.options).forEach((key) => {
-            if ('boards' === key) {
-                for (let board of this.options.boards) {
-                    this.createBoard(board.id, board.key);
-                }
-            } else {
-                let input = document.querySelector(`[data-option="${key}"]`);
-
-                if (null !== input) {
-                    input.value = this.options[key];
-                }
-            }
-        });
+        this._general.init(this.options);
     }
 
     save() {
         let jira = {
             boards: this._boards.save(),
+            checklists: [ ], // todo
         };
-        let inputOptions = document.querySelectorAll('[name="option[]"]');
 
-        for (let option of inputOptions) {
-            if ('number' === option.getAttribute('type')) {
-                this.options[option.getAttribute('data-option')] = parseInt(option.value);
-            } else {
-                this.options[option.getAttribute('data-option')] = option.value;
-            }
-        }
-
-        // this.options.boards = [];
-        //
-        // let boardId = document.querySelectorAll('[name="boardId[]"]');
-        // let boardKey = document.querySelectorAll('[name="boardKey[]"]');
-        // let boardDelete = document.querySelectorAll('[name="boardDelete[]"]');
-        //
-        // for (let key in boardKey) {
-        //     if (boardKey.hasOwnProperty(key) && false === boardDelete[key].checked) {
-        //         this.options.boards.push({
-        //             id: parseInt(boardId[key].value),
-        //             key: boardKey[key].value
-        //         });
-        //     }
-        // }
-
-        return this.options;
+        return Object.assign(jira, this._general.save());
     }
 }
--- src/js/modules/options/jira/general.mjs
+++ src/js/modules/options/jira/general.mjs
@@ -1,3 +1,30 @@
 export class General {
+    constructor() {
+        this._inputs = {
+            url: document.querySelector('#jiraUrl'),
+            maximumIssues: document.querySelector('#jiraMaximumIssues'),
+            cleanup: document.querySelector('#jiraCleanup'),
+        };
 
+        this.optionKeys = ['url', 'cleanup', 'maximumIssues'];
+
+    }
+
+    init(options) {
+        for (let key of this.optionKeys) {
+            if (options.hasOwnProperty(key)) {
+                this._inputs[key].value = options[key];
+            }
+        }
+    }
+
+    save() {
+        let general = {};
+
+        for (let key of this.optionKeys) {
+             general[key] = this._inputs[key].value;
+        }
+
+        return general;
+    }
 }
