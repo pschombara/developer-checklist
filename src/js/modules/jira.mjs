fcf6569f2c6c35085d0b48e3628e08772e0b660f
Patrick Schombara <pschombara@gmail.com>
2020-10-06 07:32:29 +0200

    feature/jdc-23
    * WIP checklists frontend (popup)

--- src/js/modules/jira.mjs
+++ src/js/modules/jira.mjs
@@ -46,6 +46,24 @@ export class Jira extends SuperJira{
             fillTab(checklist, this.tabs[number], number);
             fillContent(checklist, issue, number, this);
         }
+
+        document.addEventListener('checklistItemChecked', (e) => {
+            let unchecked = e.detail.content.querySelectorAll('[data-checked="0"]');
+            let uncheckedAll = e.detail.list.querySelectorAll('li[data-checked="0"]');
+
+            if (0 === unchecked.length) {
+                e.detail.category.setAttribute('data-checked', '1');
+                e.detail.content.classList.add('d-none');
+            } else {
+                e.detail.category.setAttribute('data-checked', '0');
+            }
+
+            if (0 === uncheckedAll.length) {
+                document.dispatchEvent(new CustomEvent('checkListAllChecked', {detail: e.detail.number}));
+            } else {
+                document.dispatchEvent(new CustomEvent('checkListEntryUnchecked', {detail: e.detail.number}));
+            }
+        });
     }
 
     getIssueTitle() {
@@ -157,13 +175,36 @@ const fillContent = (checklist, issue, number, instance) => {
         buttonSuccess.append(` ${checklist.buttons.success.text}`);
     }
 
+    if (checklist.successRequiredAll) {
+        buttonSuccess.setAttribute('disabled', 'disabled');
+
+        document.addEventListener('checkListAllChecked', (e) => {
+            if (e.detail === number ) {
+                buttonSuccess.removeAttribute('disabled');
+            }
+        })
+
+        document.addEventListener('checkListEntryUnchecked', (e) => {
+            if (e.detail === number ) {
+                buttonSuccess.setAttribute('disabled', 'disabled');
+            }
+        });
+    }
+
     if (false === checklist.buttons.failed.enabled) {
         buttonFailed.classList.add('d-none');
     } else {
         buttonFailed.append(` ${checklist.buttons.failed.text}`);
     }
 
-    for (let category of checklist.checklist) {
+    createCategories(checklist.checklist, categoryTarget, issue, number, instance);
+    registerButtonClear(buttonClear, content);
+
+    instance._checklistContent[number].appendChild(content);
+}
+
+const createCategories = (categories, categoryTarget, issue, number, instance) => {
+    for (let category of categories) {
         let newCategory = document.createElement('div');
         newCategory.innerHTML = instance._templateCategory.innerHTML;
         newCategory.innerHTML = newCategory.innerHTML.replace(new RegExp('%number%', 'g'), number);
@@ -171,10 +212,23 @@ const fillContent = (checklist, issue, number, instance) => {
 
         const categoryContent = newCategory.children[0];
         const itemTarget = categoryContent.querySelector('[data-content]');
+        const categoryTitle = categoryContent.querySelector('[data-collapse]');
+
+        categoryTitle.addEventListener('click', () => {
+            itemTarget.classList.toggle('d-none');
+        });
+
+        if (categories.length > 1) {
+            itemTarget.classList.add('d-none');
+        }
+
+        let allChecked = true;
 
         for (let item of category.items) {
             let newItem = document.createElement('li');
             const isChecked = issue.hasOwnProperty('checklist') && -1 !== issue.checklist[number].indexOf(item.id);
+
+            allChecked = allChecked && isChecked;
 
             newItem.setAttribute('data-checked', isChecked ? '1' : '0');
             newItem.setAttribute('data-uuid', item.id);
@@ -184,13 +238,37 @@ const fillContent = (checklist, issue, number, instance) => {
                 newItem.setAttribute('data-checked', '0' === newItem.getAttribute('data-checked') ? '1' : '0');
 
                 document.dispatchEvent(new CustomEvent('saveChecklist'));
+                document.dispatchEvent(new CustomEvent('checklistItemChecked',
+                    {
+                        detail: {
+                            content: itemTarget,
+                            category: categoryTitle,
+                            list: categoryTarget,
+                            number: number,
+                        }
+                    }
+                ));
             });
 
             itemTarget.append(newItem);
         }
 
+        if (allChecked) {
+            categoryTitle.setAttribute('data-checked', '1');
+        }
+
         categoryTarget.append(categoryContent);
     }
+}
 
-    instance._checklistContent[number].appendChild(content);
+const registerButtonClear = (buttonClear, content) => {
+    buttonClear.addEventListener('click', () => {
+        const checkedEntries = content.querySelectorAll('[data-checked="1"]');
+
+        for (let checkedEntry of checkedEntries) {
+            checkedEntry.setAttribute('data-checked', '0');
+        }
+
+        document.dispatchEvent(new CustomEvent('saveChecklist'));
+    });
 }
